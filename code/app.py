b72777f75d48a0a63c6c13829f605f46760f0a43
Jason Figueroa <jason.figueroa2@gmail.com>
2018-02-22 22:03:41 -0600

    added routes for category/<_id> & categories as well as templates for said routes

--- code/app.py
+++ code/app.py
@@ -21,5 +21,38 @@ def tasks():
 	tasks = json.loads(response.text)['tasks']
 	return render_template('tasks.html', tasks=tasks)
 
+@app.route('/categories')
+def categories():
+	url = 'http://localhost:5000/auth'
+	payload = {'username': 'jason', 'password': 'jason'}
+	headers = {'content-type': 'application/json'}
+	
+	response = requests.post(url, data=json.dumps(payload), headers=headers)
+	token = json.loads(response.text)['access_token']
+
+	url = 'http://localhost:5000/categories'
+	headers = {'content-type': 'application/json', 'Authorization': 'JWT {}'.format(token)}
+	
+	response = requests.get(url, headers=headers)
+	categories = json.loads(response.text)['categories']
+	return render_template('categories.html', categories=categories)
+
+@app.route('/category/<_id>')
+def category(_id):
+	url = 'http://localhost:5000/auth'
+	payload = {'username': 'jason', 'password': 'jason'}
+	headers = {'content-type': 'application/json'}
+	
+	response = requests.post(url, data=json.dumps(payload), headers=headers)
+	token = json.loads(response.text)['access_token']
+
+	url = 'http://localhost:5000/category/{}'.format(_id)
+	headers = {'content-type': 'application/json', 'Authorization': 'JWT {}'.format(token)}
+	
+	response = requests.get(url, headers=headers)
+	category_tasks = json.loads(response.text)['tasks']
+	category_name = json.loads(response.text)['name']
+	return render_template('category.html', category_tasks=category_tasks, category_name=category_name)
+
 if __name__ == '__main__':
 	app.run(debug=True, port=5025)
