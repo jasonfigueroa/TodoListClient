888363a0a30ffa29b1b36e002203b7c6cd127f18
Jason Figueroa <jason.figueroa2@gmail.com>
2018-02-24 20:42:06 -0600

    installed flask-wtf & began implementing wtforms & server-side validation

--- code/app.py
+++ code/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, json
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'jason' # change this for production, remove this if publishing source code
 bootstrap = Bootstrap(app)
 
 @app.route('/tasks')
@@ -37,7 +42,13 @@ def categories():
 	categories = json.loads(response.text)['categories']
 	return render_template('categories.html', categories=categories)
 
-@app.route('/category/<_id>')
+class PostTaskForm(FlaskForm):
+	task = StringField('Title', validators=[DataRequired(), Length(1, 6)])
+	category = StringField('Category', validators=[DataRequired(), Length(1, 6)])
+
+	submit = SubmitField('Submit')
+
+@app.route('/category/<_id>', methods=['GET', 'POST'])
 def category(_id):
 	url = 'http://localhost:5000/auth'
 	payload = {'username': 'jason', 'password': 'jason'}
@@ -52,7 +63,19 @@ def category(_id):
 	response = requests.get(url, headers=headers)
 	category_tasks = json.loads(response.text)['tasks']
 	category_name = json.loads(response.text)['name']
-	return render_template('category.html', category_tasks=category_tasks, category_name=category_name)
+	
+	newTaskTitle = None
+	newTaskCategory = None
+
+	form = PostTaskForm()
+
+	if form.validate_on_submit():
+		newTaskTitle = form.task
+		newTaskCategory = form.category
+		form.task = ''
+		form.category = ''
+
+	return render_template('category.html', category_tasks=category_tasks, category_name=category_name, form=form, newTaskTitle=newTaskTitle, newTaskCategory=newTaskCategory)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5025)
