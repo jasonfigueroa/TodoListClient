4e7331a27625a262f68504d1b6b1226921c32eb0
Jason Figueroa <jason.figueroa2@gmail.com>
2018-02-24 19:23:26 -0600

    successfully implemented PUT from JavaScript

--- code/static/js/main.js
+++ code/static/js/main.js
@@ -1,30 +1,76 @@
 $( document ).ready(function() {
+
+	const url = "http://127.0.0.1:5000";
+
     $('.task').click(function() {
+		let id = $(this).parent().attr('id');
 		$(this).toggleClass('line-through');
-		// TODO async mark as complete in db
+		let isLineThrough = $(this).hasClass('line-through');
+		$.ajax({
+			method: "POST",
+			url: `${url}/auth`,
+			contentType: "application/json",
+			dataType: "json",
+			data: JSON.stringify({
+				username: 'jason', 
+				password: 'jason'
+			})
+		})
+		.then(function(token_data) {
+			let access_token = token_data.access_token;
+			$.ajax({
+				beforeSend: function(xhr) {
+					xhr.setRequestHeader ("Authorization", `JWT ${access_token}`);
+				},
+				method: "GET",
+				url: `${url}/task/${id}`,
+				contentType: "application/json",
+				dataType: "json"
+			})
+			.then(function(task_data) {
+				isLineThrough ? task_data.complete = true : task_data.complete = false;				
+				$.ajax({
+					beforeSend: function(xhr) {
+						xhr.setRequestHeader ("Authorization", `JWT ${access_token}`);
+					},
+					method: "PUT",
+					url: `${url}/task/${id}`,
+					contentType: "application/json",
+					dataType: "json",
+					data: JSON.stringify({
+						title: task_data.title, 
+						category_name: task_data.category_name, 
+						complete: task_data.complete
+					})
+				})
+				.then(function(data) {
+					console.log(data)
+				});
+			})
+		})
 	});
 
 	$('.delete-task').click(function() {
-		var id = $(this).parent().attr('id');
+		let id = $(this).parent().attr('id');
 		$(this).parent().remove();
 		$.ajax({
 			method: "POST",
-			url: "http://127.0.0.1:5000/auth",
+			url: `${url}/auth`,
 			contentType: "application/json",
 			dataType: "json",
 			data: JSON.stringify({
 				username: 'jason',
 				password: 'jason'
-			}),
+			})
 		})
 		.then(function(token_data) {
-			var access_token = token_data.access_token;			
+			let access_token = token_data.access_token;			
 			$.ajax({
-				beforeSend: function (xhr) {
+				beforeSend: function(xhr) {
 					xhr.setRequestHeader ("Authorization", `JWT ${access_token}`);
 				},
 				method: "DELETE",
-				url: `http://127.0.0.1:5000/task/${id}`,
+				url: `${url}/task/${id}`,
 				contentType: "application/json",
 				dataType: "json"
 			})
