387923682b54ef3932607eec656db9612a5731a4
Jason Figueroa <jason.figueroa2@gmail.com>
2018-02-25 20:45:55 -0600

    added the ability to add task, add category, delete category, and show/hide forms & buttons

--- code/app.py
+++ code/app.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
+# from wtforms import validators
 
 import requests
 
@@ -26,8 +27,16 @@ def tasks():
 	tasks = json.loads(response.text)['tasks']
 	return render_template('tasks.html', tasks=tasks)
 
-@app.route('/categories')
+class PostCategoryForm(FlaskForm):
+	category = StringField('Category', validators=[DataRequired(), Length(1, 26)])
+	submit = SubmitField('Submit')
+
+@app.route('/categories', methods=['GET', 'POST'])
 def categories():
+	newCategory = None
+	
+	form = PostCategoryForm()
+
 	url = 'http://localhost:5000/auth'
 	payload = {'username': 'jason', 'password': 'jason'}
 	headers = {'content-type': 'application/json'}
@@ -35,47 +44,64 @@ def categories():
 	response = requests.post(url, data=json.dumps(payload), headers=headers)
 	token = json.loads(response.text)['access_token']
 
+	if form.validate_on_submit():
+		newCategory = form.category.data
+		form.category.data = ''
+
+		url = 'http://localhost:5000/category'
+		payload = {'name': newCategory}
+		headers = {'content-type': 'application/json', 'Authorization': 'JWT {}'.format(token)}
+		
+		response = requests.post(url, data=json.dumps(payload), headers=headers)
+	
 	url = 'http://localhost:5000/categories'
 	headers = {'content-type': 'application/json', 'Authorization': 'JWT {}'.format(token)}
 	
 	response = requests.get(url, headers=headers)
 	categories = json.loads(response.text)['categories']
-	return render_template('categories.html', categories=categories)
+	return render_template('categories.html', categories=categories, form=form)
 
 class PostTaskForm(FlaskForm):
-	task = StringField('Title', validators=[DataRequired(), Length(1, 6)])
-	category = StringField('Category', validators=[DataRequired(), Length(1, 6)])
+	task = StringField('Title', validators=[DataRequired(), Length(1, 26)])
+	category = StringField('Category', validators=[DataRequired(), Length(1, 26)])
 
 	submit = SubmitField('Submit')
 
 @app.route('/category/<_id>', methods=['GET', 'POST'])
 def category(_id):
+	newTaskTitle = None
+	newTaskCategory = None
+
+	form = PostTaskForm()
+
 	url = 'http://localhost:5000/auth'
 	payload = {'username': 'jason', 'password': 'jason'}
 	headers = {'content-type': 'application/json'}
 	
 	response = requests.post(url, data=json.dumps(payload), headers=headers)
 	token = json.loads(response.text)['access_token']
+
+	if form.validate_on_submit():
+		newTaskTitle = form.task.data
+		newTaskCategory = form.category.data
+
+		form.task.data = ''
+		form.category.data = ''
+
+		url = 'http://localhost:5000/task'
+		payload = {'title': newTaskTitle, 'category_name': newTaskCategory}
+		headers = {'content-type': 'application/json', 'Authorization': 'JWT {}'.format(token)}
+		
+		response = requests.post(url, data=json.dumps(payload), headers=headers)
 
 	url = 'http://localhost:5000/category/{}'.format(_id)
 	headers = {'content-type': 'application/json', 'Authorization': 'JWT {}'.format(token)}
 	
 	response = requests.get(url, headers=headers)
 	category_tasks = json.loads(response.text)['tasks']
-	category_name = json.loads(response.text)['name']
-	
-	newTaskTitle = None
-	newTaskCategory = None
+	category_name = json.loads(response.text)['name']	
 
-	form = PostTaskForm()
-
-	if form.validate_on_submit():
-		newTaskTitle = form.task
-		newTaskCategory = form.category
-		form.task = ''
-		form.category = ''
-
-	return render_template('category.html', category_tasks=category_tasks, category_name=category_name, form=form, newTaskTitle=newTaskTitle, newTaskCategory=newTaskCategory)
+	return render_template('category.html', category_tasks=category_tasks, category_name=category_name, form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5025)
--- code/static/js/main.js
+++ code/static/js/main.js
@@ -2,6 +2,28 @@ $( document ).ready(function() {
 
 	const url = "http://127.0.0.1:5000";
 
+	$('#add-task-button').click(function() {
+		$(this).addClass('hidden');
+		$('#add-task-form').removeClass('hidden');
+	});
+
+	$('#cancel-add-task-button').click(function(event) {
+		event.preventDefault();
+		$('#add-task-form').addClass('hidden');
+		$('#add-task-button').removeClass('hidden');
+	});
+
+	$('#add-category-button').click(function() {
+		$(this).addClass('hidden');
+		$('#add-category-form').removeClass('hidden');
+	});
+
+	$('#cancel-add-category-button').click(function(event) {
+		event.preventDefault();
+		$('#add-category-form').addClass('hidden');
+		$('#add-category-button').removeClass('hidden');
+	});
+
     $('.task').click(function() {
 		let id = $(this).parent().attr('id');
 		$(this).toggleClass('line-through');
@@ -71,6 +93,36 @@ $( document ).ready(function() {
 				},
 				method: "DELETE",
 				url: `${url}/task/${id}`,
+				contentType: "application/json",
+				dataType: "json"
+			})
+			.then(function(data) {
+				console.log(data);
+			});
+		});
+	});
+
+	$('.delete-category').click(function() {
+		let id = $(this).parent().attr('id');
+		$(this).parent().remove();
+		$.ajax({
+			method: "POST",
+			url: `${url}/auth`,
+			contentType: "application/json",
+			dataType: "json",
+			data: JSON.stringify({
+				username: 'jason',
+				password: 'jason'
+			})
+		})
+		.then(function(token_data) {
+			let access_token = token_data.access_token;			
+			$.ajax({
+				beforeSend: function(xhr) {
+					xhr.setRequestHeader ("Authorization", `JWT ${access_token}`);
+				},
+				method: "DELETE",
+				url: `${url}/category/${id}`,
 				contentType: "application/json",
 				dataType: "json"
 			})
